ff5a48604570a901ef1d138063c22c663a67bbef
Taja House <ahlliahtaja@yahoo.com>
2020-09-13 18:19:17 -0700

    a complete in comp.py

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -24,7 +24,7 @@ humans = [
 # Write a list comprehension that creates a list of names of everyone
 # whose name starts with 'D':
 print("Starts with D:")
-a = []
+a = [i.name for i in humans if i.name.startswith("D")]
 print(a)
 
 # Write a list comprehension that creates a list of names of everyone
