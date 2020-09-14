b9f3f8cd4f2ae3c68de1ce494b25a5888823fc48
Taja House <ahlliahtaja@yahoo.com>
2020-09-13 18:11:34 -0700

    oop1 test passed

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,32 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+#a this is the root
+class Vehicle:
+    pass
+
+#a --- the root is Vehicle
+class FlightVehicle(Vehicle):
+    def __init__(self):
+        pass
+
+#a -- the root is FlightVehicle
+class Airplane(FlightVehicle):
+    pass
+
+#a -- the root is Vehicle
+class GroundVehicle(Vehicle):
+    pass
+
+#a --- the root is GroundVehicle
+class Car(GroundVehicle):
+    pass
+
+#a -- the root is GroundVehicle
+class Motorcycle(GroundVehicle):
+    pass
+
+#a -- the root is FlightVehicle
+class Starship(FlightVehicle):
+    pass
